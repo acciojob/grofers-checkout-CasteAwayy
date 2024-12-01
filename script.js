69a03763162885c80353feaefd490eb56f7eb4bd
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-01 18:22:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const getSumBtn = document.createElement("button");
+const getAllprice = document.querySelectorAll("td:last-child");
+const table = document.querySelector("#table");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  //Add your code here
+  let totalSum = 0;
+  for (let i of getAllprice) totalSum += parseFloat(i.textContent);
+  const row = table.insertRow(table.children[1].childElementCount);
+  const totalPrice = row.insertCell(0);
+  const price = row.insertCell(1);
+  totalPrice.textContent = "Total Price";
+  price.textContent = `${totalSum}`;
 };
 
 getSumBtn.addEventListener("click", getSum);
-
